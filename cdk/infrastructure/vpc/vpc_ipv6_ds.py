f593cf8b0077b5e5d5eb15e691bfb171a9cc2dc9
Wolfgang Unger <wolfgang.unger@gmx.de>
2024-02-15 17:21:40 -0300

    cdk

--- cdk/infrastructure/vpc/vpc_ipv6_ds.py
+++ cdk/infrastructure/vpc/vpc_ipv6_ds.py
@@ -3,6 +3,7 @@ from aws_cdk import (
     aws_ec2 as ec2,
     Fn   
 )
+#https://docs.aws.amazon.com/cdk/api/v2/python/aws_cdk.aws_ec2/Vpc.html
 
 def associate_subnet_with_v6_cidr(vpc, count, subnet):    
     cfn_subnet = subnet.node.default_child
@@ -22,7 +23,22 @@ class VPCIPv6DualStack(ec2.Vpc):
             scope, 
             id, 
             ip_addresses= ec2.IpAddresses.cidr(cidr_range),
-            #ipv6Addresses=
+            ip_protocol=ec2.IpProtocol.DUAL_STACK,
+            subnet_configuration=[
+                    ec2.SubnetConfiguration(
+                    subnet_type=ec2.SubnetType.PUBLIC,
+                    name='Public A',
+                    cidr_mask=20,
+                    ipv6_assign_address_on_creation=True,
+                    map_public_ip_on_launch=True
+                    ), 
+                    ec2.SubnetConfiguration(
+                    subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS,
+                    name='Private A',
+                    cidr_mask=20,
+                    ipv6_assign_address_on_creation=True
+                    ), 
+                    ],
             create_internet_gateway=True,
             nat_gateway_provider=None,
             nat_gateways = 0,
@@ -30,33 +46,33 @@ class VPCIPv6DualStack(ec2.Vpc):
             **kwargs)
 
         # make an ipv6 cidr
-        ipv6cidr = ec2.CfnVPCCidrBlock(self, "CIDR6",
-            vpc_id=self.vpc_id,
-            amazon_provided_ipv6_cidr_block=True
-        )
+        # ipv6cidr = ec2.CfnVPCCidrBlock(self, "CIDR6",
+        #     vpc_id=self.vpc_id,
+        #     amazon_provided_ipv6_cidr_block=True
+        # )
         ## egress only igw
-        cfn_egress_only_internet_gateway = ec2.CfnEgressOnlyInternetGateway(self, "EgressOnly IGW",
-            vpc_id=self.vpc_id,
-        )
-
+        # cfn_egress_only_internet_gateway = ec2.CfnEgressOnlyInternetGateway(self, "EgressOnly IGW",
+        #     vpc_id=self.vpc_id,
+        # )
+        
         # connect the ipv6 cidr to all vpc subnets
-        subnetcount = 0
-        for pubsubnet in self.public_subnets: 
-            pubsubnet.node.add_dependency(ipv6cidr)
-            pubsubnet.type = ec2.SubnetType.PUBLIC
-            pubsubnet.name = "Public " + str(subnetcount)
-            associate_subnet_with_v6_cidr(self, subnetcount, pubsubnet)
-            #pubsubnet.add_ipv6_default_internet_route(self.internet_gateway_id)
-            #pubsubnet.add_route("IPv6", router_id=self.internet_gateway_id, router_type="InternetGateway",destination_ipv6_cidr_block="" )
-            subnetcount = subnetcount + 1  
-        for privsubnet in self.private_subnets: 
-            privsubnet.node.add_dependency(ipv6cidr)
-            privsubnet.type = ec2.SubnetType.PRIVATE_WITH_EGRESS
-            privsubnet.name = "Private" + str(subnetcount- 2)
-            associate_subnet_with_v6_cidr(self, subnetcount, privsubnet)
-            #privsubnet.add_route("Default", router_id=cfn_egress_only_internet_gateway.ref, router_type="EgressOnlyInternetGateway")
-            privsubnet.add_ipv6_default_egress_only_internet_route(cfn_egress_only_internet_gateway)
-            subnetcount = subnetcount + 1     
+        # subnetcount = 0
+        # for pubsubnet in self.public_subnets: 
+        #     pubsubnet.node.add_dependency(ipv6cidr)
+        #     pubsubnet.type = ec2.SubnetType.PUBLIC
+        #     pubsubnet.name = "Public " + str(subnetcount)
+        #     associate_subnet_with_v6_cidr(self, subnetcount, pubsubnet)
+        #     #pubsubnet.add_ipv6_default_internet_route(self.internet_gateway_id)
+        #     subnetcount = subnetcount + 1  
+        #     pubsubnet.tags = [{"key": "Name", "value": "Public " + str(subnetcount)}]
+        # for privsubnet in self.private_subnets: 
+        #     privsubnet.node.add_dependency(ipv6cidr)
+        #     privsubnet.type = ec2.SubnetType.PRIVATE_WITH_EGRESS
+        #     privsubnet.name = "Private" + str(subnetcount- 2)
+        #     privsubnet.tags = [{"key": "Name", "value": "Private " + str(subnetcount)}]
+        #     associate_subnet_with_v6_cidr(self, subnetcount, privsubnet)
+        #     #privsubnet.add_ipv6_default_egress_only_internet_route(cfn_egress_only_internet_gateway.attr_id)
+        #     subnetcount = subnetcount + 1     
 
 
 
